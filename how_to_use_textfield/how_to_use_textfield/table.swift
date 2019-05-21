34500778fa224f126569771ebc0d1e430b5ed126
cyc2303 <cyc0723@naver.com>
2019-05-21 23:10:52 +0900

    Yechan Updated

--- how_to_use_textfield/how_to_use_textfield/table.swift
+++ how_to_use_textfield/how_to_use_textfield/table.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 class table: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return YunniArray.count
+        return YunniArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //tableView.reloadData()
         let cell = tableView.dequeueReusableCell(withIdentifier: "asdf", for: indexPath)
         cell.textLabel?.text = String(indexPath.row)
         cell.detailTextLabel?.text = YunniArray[indexPath.row].id
@@ -23,6 +26,7 @@ class table: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //tableView?.reloadData() 에러나서 이렇게 해놓음
         // Do any additional setup after loading the view.
     }
     
