3a234d6190200f051dd84d7c970d75264f5b58ec
cyc2303 <cyc0723@naver.com>
2019-06-13 17:00:29 +0900

    Add LaunchScreen

---
+++ how_to_use_textfield/how_to_use_textfield/LanchVC.swift
@@ -0,0 +1,31 @@
+//
+//  LanchVC.swift
+//  how_to_use_textfield
+//
+//  Created by Choi Yechan on 13/06/2019.
+//  Copyright © 2019 Choi Yechan. All rights reserved.
+//
+
+import UIKit
+
+class LanchVC: UIViewController {
+
+    @IBOutlet weak var text: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        text.text = "Lanch"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
